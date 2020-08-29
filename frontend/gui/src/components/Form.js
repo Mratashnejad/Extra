75dc67edf16d09148051ab3a4bcd84143efeb01f
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-08-29 07:31:30 +0400

    add successfuly items to form

--- frontend/gui/src/components/Form.js
+++ frontend/gui/src/components/Form.js
@@ -10,14 +10,15 @@ class CustomForm extends React.Component {
       event.preventDefault();
       const title = event.target.elements.title.value;
       const content = event.target.elements.content.value;
+    const manager = event.target.elements.manager.value;
 
       switch (requestType) {
         case 'post':
-          return axios.post('http://127.0.0.1:8000/api/', {title:title,content:content})
+          return axios.post('http://127.0.0.1:8000/api/', {title:title,content:content,manager:manager})
             .then(res => console.log(res))
             .catch(error => console.err(error));
         case 'put':
-          return axios.put(`http://127.0.0.1:8000/api/${articleID}/`, {title:title,content:content})
+          return axios.put(`http://127.0.0.1:8000/api/${articleID}/`, {title:title,content:content,manager:manager})
             .then(res => console.log(res))
             .catch(error => console.err(error));
 
@@ -36,10 +37,13 @@ class CustomForm extends React.Component {
           this.props.articleID
         )}>
           <FormItem label="Title">
-            <Input name='title' placeholder="Enter title here" />
+            <Input name='title' placeholder="Enter title here :" />
           </FormItem>
           <FormItem label="Content">
-            <Input name='content' placeholder="Enter some content" />
+            <Input name='content' placeholder="Enter some content :" />
+          </FormItem>
+          <FormItem label="Manager">
+            <Input name='manager' placeholder="Enter manager name :" />
           </FormItem>
           <FormItem>
             <Button type="primary" htmlType="Submit">{this.props.btnText}</Button>
