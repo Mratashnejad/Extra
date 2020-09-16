5781bddc18edc613dd3b381d7e2e3a35f2b0c22e
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-09-16 08:57:08 +0400

    update View.py

--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -8,6 +8,18 @@ from articles.models import Article
 
 from .serializes import ArticleSerializer
 
+from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.instagram.views import InstagramOAuth2Adapter
+
+class FacebookLogin(SocialLoginView):
+    adapter_class = FacebookOAuth2Adapter
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+class InstagramLogin(SocialLoginView):
+    adapter_class = InstagramOAuth2Adapter
+
+
 
 class ArticleViewSet (viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
