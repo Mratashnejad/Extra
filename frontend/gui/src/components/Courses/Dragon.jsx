c3f566d38652737b2e69ef34d82495714a3893c8
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-11-16 06:59:16 +0400

    add dragon

---
+++ frontend/gui/src/components/Courses/Dragon.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Dragon extends React.Component {
+    render() {
+        return (
+
+            <p>this is Dragon page</p>
+        )
+    }
+}
+export default Dragon
