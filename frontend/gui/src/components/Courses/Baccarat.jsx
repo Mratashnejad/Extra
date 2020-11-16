2d3ca00d3afe82721f6dbd57b5b9556dc4755848
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-11-16 06:58:30 +0400

    add baccarat page

---
+++ frontend/gui/src/components/Courses/Baccarat.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Baccarat extends React.Component {
+    render() {
+        return (
+
+            <p>this is Baccarat page</p>
+        )
+    }
+}
+export default Baccarat
--- frontend/gui/src/components/Courses/Poker.jsx
+++ frontend/gui/src/components/Courses/Poker.jsx
@@ -1,1 +1,11 @@
 import React from "react";
+
+class Poker extends React.Component {
+    render() {
+        return (
+
+            <p>this is poker page</p>
+        )
+    }
+}
+export default Poker
