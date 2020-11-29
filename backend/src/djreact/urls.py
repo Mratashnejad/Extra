9af5d88e115cf6a2d86ec5fe1ec355ce14c7b7ee
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-11-29 17:33:58 +0400

    urls admin

--- backend/src/djreact/urls.py
+++ backend/src/djreact/urls.py
@@ -1,11 +1,19 @@
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include , re_path
+from rest_framework import routers
+
+
+
+
+router = routers.DefaultRouter()
+
 
 
 urlpatterns = [
  
     path('admin/', admin.site.urls),
-    path('',include('accounts.api.urls')),
-    path('Extra/',include('Extra.urls'),name='Extra'),
+    path('api/auth/',include('accounts.api.urls')),
+    path('api/',include(router.urls)),
     
 ]
