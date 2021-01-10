eb5c54dcc645e10734cf7d25444d611c3e36d289
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2021-01-10 21:06:29 +0400

    update

--- backend/src/cassino/api/urls.py
+++ backend/src/cassino/api/urls.py
@@ -1,13 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import (UserIDView,ExtraShiftListView,DealersListView)
 
+from cassino.api.views import ExtrashiftView
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('user-id/',UserIDView.as_view(),name='user-id'),
-    path('extra/',ExtraShiftListView.as_view(),name='extra-list'),
-    path('dealers/',DealersListView.as_view(),name='Dealers-list')
-    # path('api/', include('articles.api.urls')),
-    # path('rest-auth/', include('rest_auth.urls')),
-    # path('api-auth/', include('rest_framework.urls')),
-]
+router = DefaultRouter()
+router.register(r'',ExtrashiftView,basename='Extra')
+urlpatterns = router.urls
+
